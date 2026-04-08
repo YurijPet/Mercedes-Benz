4bf3e67b2a145ef04271db04bf42a51896ca1e4c
YurijPet <166520788+YurijPet@users.noreply.github.com>
2026-04-08 19:02:02 +0100

    Accordion
    Accordion for the vehicle's technical specifications unit

---
+++ js/accordion.js
@@ -0,0 +1,23 @@
+const lists = document.querySelectorAll(".feature-sub");
+const btns = document.querySelectorAll(".feature__link");
+
+// btn.addEventListener('click', () => {
+//     alert('click!')
+// })
+//
+
+btns.forEach((btnItem, index) => {
+  btnItem.addEventListener("click", () => {
+    btns.forEach((btnItem) => {
+      btnItem.classList.remove("feature__link_active");
+    });
+    btnItem.classList.add("feature__link_active");
+  
+
+    lists.forEach((listItem, index) => {
+      listItem.classList.add('hidden');
+    });
+    lists[index].classList.remove('hidden')
+
+  });
+});
